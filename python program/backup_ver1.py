be1c3446e77b4c7bd0e4542f54cece273df4005d
Jon <1575456904@qq.com>
2016-04-27 11:23:26 +0800

    增加20160427的程序

--- python program/backup_ver1.py
+++ python program/backup_ver1.py
@@ -6,13 +6,14 @@ import time
 
 # 1. The files and directions to be backed up are specified in a list
 source = 'G:\\learnpython\\readme.md'
-
+# 2. The backup must be stored in a main backup directory
 target_dir = 'G:'
-
+# 3. The files are backed up into a rar file.
+# 4. The name of the zip archive is the current date and time
 target = target_dir + time.strftime('%Y%m%d%H%M%S') + '.rar'
 
 rar_command = "WinRAR a %s %s -r" % (target, source)
-
+# Run the backup
 if os.system(rar_command) == 0:
     print 'Successful backup to', target
 else:
