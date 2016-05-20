7a9cac7cc4dd0cc181b917b5c2ef2ab80ce9da4b
Jon <1575456904@qq.com>
2016-05-20 15:07:30 +0800

    增加20160520程序

---
+++ python program/examples/urllib2_test01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Filename: urllib2_test01.py
+
+# -*- coding: utf-8 -*-
+
+import urllib2  
+response = urllib2.urlopen('http://www.baidu.com/')  
+html = response.read()  
+print html  
