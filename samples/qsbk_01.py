91611491c0fd9adf7a115928388c034b0642c92e
Jon <1575456904@qq.com>
2016-05-25 13:02:57 +0800

    重设程序保存路径

--- samples/qsbk_01.py
+++ samples/qsbk_01.py
@@ -7,21 +7,22 @@ import urllib
 import urllib2
 import re
 
-page = 1
-url = 'http://www.qiushibaike.com/hot/page/' + str(page)
+page = raw_input("please enter the page number:")
+url = 'http://www.qiushibaike.com/8hr/page/'+ page +'/?s=4880477' 
+
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = { 'User-Agent' : user_agent }
 try:
     request = urllib2.Request(url,headers = headers)
     response = urllib2.urlopen(request)
     content = response.read().decode('utf-8')
-    pattern = re.compile('<div.*?author\sclearfix">.*?<img.*?>.*?<h2>(.*?)</h2>.*?<div.*?'+
+    pattern = re.compile('<div.*?author.*?>.*?<img.*?>.*?<h2>(.*?)</h2>.*?<div.*?'+
                          'content">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
     items = re.findall(pattern,content)
     for item in items:
         haveImg = re.search("img",item[2])
         if not haveImg:
-            print item[0],item[1],item[3]
+            print item[0],item[3],item[1]
 except urllib2.URLError, e:
     if hasattr(e,"code"):
         print e.code
