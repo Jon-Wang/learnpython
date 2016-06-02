48de7dd750f2b77ee21aee43be477c3ee35277ba
Jon <1575456904@qq.com>
2016-06-02 10:53:57 +0800

    更新正则表达式匹配

--- practice/bdtb_01.py
+++ practice/bdtb_01.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/Python
 # filename: bdtb_01.py
 
 # -*- coding: utf-8 -*-
@@ -7,7 +7,7 @@ import urllib
 import urllib2
 import re
 
-class tool:
+class Tool:
     removeImg = re.compile('<img.*?>| {7}|')
     removeAddr = re.compile('<a.*?>|</a>')
     replaceLine = re.compile('<tr>|<div>|</div>|</p>')
@@ -27,14 +27,14 @@ class tool:
 
         return x.strip()
     
-class bdtb:
+class Bdtb:
 
     def __init__(self,seeLZ = 1):
         self.baseURL = 'http://tieba.baidu.com/p/3138733512'
         self.seeLZ = '?see_lz='+str(seeLZ)
-        self.tool = tool()
+        self.tool = Tool()
         
-    def getpage(self,pageNum = 1):
+    def getPage(self,pageNum = 1):
         try:
             url = self.baseURL+ self.seeLZ + '&pn=' + str(pageNum)
             request = urllib2.Request(url)
@@ -47,31 +47,33 @@ class bdtb:
                 print e.reason
                 return None
 
-    def gettitle(self,page):
-        pattern = re.compile('<title>(.*?)_nba.*?</title>',re.S)
+    def getTitle(self,page):
+        pattern = re.compile('<title>(.*?)</title>',re.S)
         result = re.search(pattern,page)
         if result:
-            print "title: %s" % (result.group(1).strip())
+            print "Title: %s" % (result.group(1).strip())
         else:
             return None
         
-    def getpagenum(self,page):
-        pattern = re.compile('<input theme="4" id="jumpPage4" max-page="(.*?)" type="text"',re.S)
+    def getPageNum(self,page):
+        pattern = re.compile('<li class="l_reply_num.*?</span>.*?<span.*?>(.*?)</span>',re.S)
         result = re.search(pattern,page)
         if result:
-            print "pagenum: %s" % (result.group(1).strip())
+            print "PageNum: %s" % (result.group(1).strip())
         else:
             return None
         
-    def getcontent(self,page):
+    def getContent(self,page):
         pattern = re.compile('<div id="post_content_.*?>(.*?)</div>',re.S)
         items = re.findall(pattern,page)
+        floor = 1
         for item in items:
-            #print item
-            print self.tool.replace(item)
+            print floor,u"floor-------------------------------\n"
+            print self.tool.replace(item),"\n"
+            floor += 1
         
-bdtb = bdtb()
-page = bdtb.getpage()
-bdtb.gettitle(page)
-bdtb.getpagenum(page)
-bdtb.getcontent(page)
+bdtb = Bdtb()
+page = bdtb.getPage()
+bdtb.getTitle(page)
+bdtb.getPageNum(page)
+bdtb.getContent(page)
